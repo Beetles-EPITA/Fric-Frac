85aafb0a55c06cd200e90df24667d3571b4469d9
nicolas <nicolas.prevost@epita.fr>
2021-03-03 22:38:46 +0100

    [*] Fence and grass texture added

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -26,7 +26,7 @@ namespace Manager
         private void CreateController()
         {
             Debug.Log("Instantiated Player Controller");
-            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player", "PlayerController"), new Vector3(177, 0, -315), 
+            PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Player", "PlayerController"), new Vector3(137, 19.5f, -325), 
                 Quaternion.identity);
         }
 
