7e5c5a46ed89a3719a9dcedcfe5400940f146799
Thomas LO IACONO <thomas.lo-iacono@epita.fr>
2021-03-03 16:12:31 +0100

    menu Settings terminé

--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -27,13 +27,27 @@ public class SettingsMenu : MonoBehaviour
         
         List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
         
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetVolume(float volume)
