a76745c97970852872fee3cea7a595a44342bbc4
dylou <dylan.innou@epita.fr>
2021-03-04 16:23:48 +0100

    [+] animation solo
    add the preexisting animation in the solo play

--- Assets/Scripts/Solo/NaveMeshPlayerMotor.cs
+++ Assets/Scripts/Solo/NaveMeshPlayerMotor.cs
@@ -12,13 +12,28 @@ public class NaveMeshPlayerMotor : MonoBehaviour
     [SerializeField]
     public Component player;
 
+    //Animation :
+    private Animator anim;
+    private float speed;
+    
     // Update is called once per frame
     void Update()
     {
+        anim = GetComponent<Animator>();
         
         if (IaStatesMachine.Distance(agent, player, 225f) && !IaStatesMachine.IsObjectBetween(agent, player))
         {
             agent.SetDestination(player.transform.localPosition);
         }
+
+        if (agent.hasPath)
+        {
+            anim.SetFloat("Speed", 3);
+        }
+        else
+        {
+            anim.SetFloat("Speed", 0);
+        }
+
     }
 }
