119daecf4acb3432a64ae8d88826357153431b06
Valentin Uhlrich <valentin.uhlrich@epita.fr>
2021-06-18 16:57:20 +0200

    [+] Input Menu + Save Input Settings

---
+++ Assets/Scripts/Menus/InputMenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Menus
+{
+    public class InputMenu : MonoBehaviour
+    {
+        private Event keyEvent;
+        private Text buttonText;
+        private KeyCode newKey;
+
+        private bool waitingForKey;
+        
+        private void Start()
+        {
+            gameObject.SetActive(false);
+            waitingForKey = false;
+            foreach (var componentsInChild in GetComponentsInChildren<Button>())
+            {
+                if (Enum.TryParse(componentsInChild.gameObject.name, out GameManager.KeyType type))
+                {
+                    componentsInChild.GetComponentInChildren<TMP_Text>().text =
+                        GameManager.Instance.inputs[type].ToString();
+                }
+            }
+        }
+    }
+}
