0178a02a9620b13218d737b9fff7e5be2c40dc84
nicolas <nicolas.prevost@epita.fr>
2021-06-20 17:49:39 +0200

    [*] trying things

--- Assets/Scripts/Solo/IAManager.cs
+++ Assets/Scripts/Solo/IAManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Manager;
+using Photon.Pun;
 using UnityEngine;
+using Random = System.Random;
 
 public class IAManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "IA", "IAController"), PlayerManager.RandomPosition(), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("debug");
+        
     }
 }
