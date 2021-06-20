2c086e665dd45a4d9238a684e08acc33f2b88876
nicolas <nicolas.prevost@epita.fr>
2021-06-20 19:39:03 +0200

    [+]IA script

--- Assets/Scripts/Solo/IAManager.cs
+++ Assets/Scripts/Solo/IAManager.cs
@@ -8,10 +8,11 @@ using Random = System.Random;
 
 public class IAManager : MonoBehaviour
 {
+    Random random = new Random();
     // Start is called before the first frame update
     void Start()
     {
-        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "IA", "IAController"), PlayerManager.RandomPosition(), Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("Prefabs", "IA", "IAController"), new Vector3(random.Next(12) + 93, 18, random.Next(9) - 284), Quaternion.identity);
     }
 
     // Update is called once per frame
